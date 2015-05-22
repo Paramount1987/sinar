f378b0dae7d1829b9de2eb6f724564b967e699eb
Paramount2009 <Paramount2009@ya.ru>
2015-05-22 14:11:53 +0300

    first commit

---
+++ js/sinar.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+/////////////////////////////
+			// $('#wrapper').fullpage({
+			// 	scrollOverflow: true
+			// });
+///////////////////////////////
+$(".fancybox").fancybox({
+    openEffect  : 'elastic',
+    closeEffect : 'elastic'
+  });
+/////////////////////////////////
+
+});
